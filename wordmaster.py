8ca4372c12790b56174b698c1894865032068878
Ahnaf Chowdhury <34480805+ahnaf-chowdhury@users.noreply.github.com>
2022-09-02 21:31:33 +0600

    Update wordmaster.py

--- wordmaster.py
+++ wordmaster.py
@@ -1,7 +1,33 @@
+'''
+WORDMASTER
+
+A word guessing game similar to Wordle (https://www.nytimes.com/games/wordle/index.html)
+that runs on the command line.
+The user has six tries to guess a five-letter word the program has randomly chosen.
+
+During gameplay, the program highlights letters that are correct or incorrect by
+marking correct letters in the correct position green (and capitalised), correct
+letters in the incorrect position yellow (and capitalised) and prints incorrect
+letters in the default font colour of the console.
+
+In addition to displaying the guessed word with letters highlighted appropriately,
+the program also displays a virtual qwerty keyboard that marks used letters with
+a strikethrough, and displays them with the green-yellow-default colout scheme
+that has been described.
+
+This module contains two classes:
+Keyboard - which helps in displaying the virtual keyboard appropriately, and
+Game - which help initiating and progressing through a game.
+
+The game can be played by running this script directly or by importing the Keyboard
+and Game classes from this module on to another script.
+'''
+
 import enchant
 import requests as req
 import random
 import os.path
+from typing import Tuple, List
 
 class Keyboard:
     def __init__(self):
@@ -19,54 +45,17 @@ class Keyboard:
 
     def print(self):           # print keyboard
         for i, line in enumerate(self.keys):
-            print(i*" ", end="")
+            print(i*" ", end="")             # spacing to match a real keyboard's arrangement
             for key in line:
                 print(key, end=' ')
             print("")
 
-    def mark(self, char, colour):            # marks letters/keys
+    def mark(self, char:str, colour:str):            # marks letters/keys
         for i in range(len(self.keys)):
             for j in range(len(self.keys[i])):
                 if self.keys[i][j][0] == char:
                     if self.keys[i][j][0:2] != "\0":  # not already changed
                         self.keys[i][j] = self.attributes[colour] + self.keys[i][j] + self.attributes['striketh'] + self.attributes['end']
-
-def load_dictionary2list(filename):
-    '''reads dict of 5 letter words onto a list'''
-
-    # spellchecker
-    dictionary = enchant.Dict(LANG) # spellchecker
-
-    if not os.path.isfile(filename):      # download file if it's not there
-        url = "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
-        res = req.get(url)
-        with open(filename, 'wb') as f:
-            f.write(res.content)
-
-    flw_file = open(filename)
-    flw_list = []
-    for line in flw_file:        # count will have index of last word
-        if dictionary.check(line[:-1]):        # to avoid words that the spellchecker doesnt know of
-            flw_list.append(line[:-1])
-    flw_file.close()
-    return flw_list, len(flw_list)-1
-
-def word2dict(word):
-    '''converts word to dict representing letter counts'''
-    output = dict()
-    for ch in word:
-        if ch in output:
-            output[ch] += 1
-        else:
-            output[ch] = 1
-    return output
-
-def print_results(res):
-    '''print results in a 5x6 grid'''
-    for line in res:
-        for ch in line:
-            print(ch, end=" ")
-        print("")
 
 class Game:
     def __init__(self):
@@ -75,12 +64,10 @@ class Game:
         self.guess = None
         self.results = [["_" for _ in range(5)] for _ in range(6)]
         self.kb = Keyboard()
-
-        self.flw, self.count = load_dictionary2list('five_letter_words.txt')   # load dictionary of 5-letter words
-        self.true = self.flw[random.randint(0, self.count)]              # word to guess
-        self.true_dict = word2dict(self.true)
-        self.dictionary = enchant.Dict(LANG) # spellchecker
-
+        self.words_list, self.count = self.load_dictionary2list('five_letter_words.txt')   # load dictionary of 5-letter words
+        self.true_word = self.words_list[random.randint(0, self.count)]              # word to guess
+        self.true_word_dict = self.word2dict(self.true_word)
+        self.dictionary = enchant.Dict(LANG)                              # spellchecker
         self.attributes = {'green':'\033[92m',
               'yellow':'\033[93m',
               'bold':'\033[1m',
@@ -88,70 +75,83 @@ class Game:
               'end':'\033[0m',
               'striketh':'\u0336'}   # use strikethrough after string
 
-    def reset_game(self):
+    def load_dictionary2list(self, filename:str) -> Tuple[List[str], int]:
+        '''reads dict of 5 letter words onto a list'''
+
+        dictionary = enchant.Dict(LANG) # spellchecker
+
+        if not os.path.isfile(filename):      # download file if it's not there
+            url = "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
+            res = req.get(url)
+            with open(filename, 'wb') as f:
+                f.write(res.content)
+
+        words_file = open(filename)
+        words_list = []
+        for line in words_file:        # count will have index of last word
+            if dictionary.check(line[:-1]):        # to avoid words that the spellchecker doesnt know of
+                words_list.append(line[:-1])
+        words_file.close()
+        return words_list, len(words_list)-1
+
+    def reset_game(self) -> None:
         self.tries = 0
         self.found = False
         self.guess = None
         self.results = [["_" for _ in range(5)] for _ in range(6)]
         self.kb = Keyboard()
 
-        self.true = self.flw[random.randint(0, self.count)]              # word to guess
-        self.true_dict = word2dict(self.true)
+        self.true_word = self.words_list[random.randint(0, self.count)]              # word to guess
+        self.true_word_dict = self.word2dict(self.true_word)
 
-    def play(self):
+    def play(self) -> None:
 
-        while not self.found and self.tries < 6 and self.guess != '0':
+        while not self.found and self.tries < 6 and self.guess != '0':   # '0' is the code for terminating the program
 
             self.guess = self.get_guess()      # get a valid Guess
 
             if self.guess != '0':
-                self.check_exact_matches()
+                self.check_exact_matches()     # exact matches are prioritised
                 self.check_other_matches()
 
-                print_results(self.results)
+                self.print_results()
                 self.kb.print()
 
                 self.tries += 1
 
-                if not self.true_dict:
+                if not self.true_word_dict:
                     self.found = True
                     print('Correct guess! Number of tries = ' + str(self.tries))
 
-                self.true_dict = word2dict(self.true)      # reset true_dict
+                self.true_word_dict = self.word2dict(self.true_word)      # reset true_dict
 
         if self.guess == '0':
             print('QUIT')
         elif not self.found:
-            print(':( The word was', self.true)
+            print(':( The word was', self.true_word)
 
-    def check_exact_matches(self):
+    def check_exact_matches(self) -> None:
         for i in range(5):
-            if self.guess[i] == self.true[i]:
+            if self.guess[i] == self.true_word[i]:
                 self.results[self.tries][i] = self.attributes['green'] + self.attributes['bold'] + self.guess[i].upper() + self.attributes['end']
                 self.kb.mark(self.guess[i], 'green')
-                self.update_dict(self.guess[i])         # update letter count for true word
+                self.update_true_word_dict(self.guess[i])         # update letter count for true word
             else:
                 self.results[self.tries][i] = "_"
 
-    def check_other_matches(self):
+    def check_other_matches(self) -> None:
         for i in range(5):
             if self.results[self.tries][i] == "_":
-                if self.guess[i] in self.true_dict:
+                if self.guess[i] in self.true_word_dict:
                     self.results[self.tries][i] = self.attributes['yellow'] + self.attributes['bold'] + self.guess[i].upper() + self.attributes['end']
                     self.kb.mark(self.guess[i], 'yellow')
-                    self.update_dict(self.guess[i])         # update letter count for true word
+                    self.update_true_word_dict(self.guess[i])         # update letter count for true word
 
                 else:
                     self.results[self.tries][i] = self.guess[i]
                     self.kb.mark(self.guess[i], 'none')
 
-    def update_dict(self, letter):
-        if self.true_dict[letter] == 1:      # update dict
-            self.true_dict.pop(letter)
-        else:
-            self.true_dict[letter] -= 1
-
-    def get_guess(self):
+    def get_guess(self) -> str:
         guess = None
         while not guess:
             guess = input('Please enter a guess (5 letters):  (or enter 0 to quit)')
@@ -164,11 +164,31 @@ class Game:
                     guess = None
         return guess
 
+    def word2dict(self, word:str) -> dict:
+        '''converts word to dict representing letter counts'''
+        output = dict()
+        for ch in word:
+            if ch in output:
+                output[ch] += 1
+            else:
+                output[ch] = 1
+        return output
+
+    def update_true_word_dict(self, letter:str) -> None:
+        if self.true_word_dict[letter] == 1:      # update dict
+            self.true_word_dict.pop(letter)
+        else:
+            self.true_word_dict[letter] -= 1
+
+    def print_results(self) -> None:
+        '''print results in a 5x6 grid'''
+        for line in self.results:
+            for ch in line:
+                print(ch, end=" ")
+            print("")
+
 
 if __name__ == "__main__":
-
-    # # load 5-letter-word dictionary
-    # flw = load_dictionary2list('five_letter_words.txt')
 
     LANG = "en_US"          # the 5-letter-word list contains "en_US" words
 
