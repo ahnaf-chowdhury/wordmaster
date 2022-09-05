41f0377f8fc2b2c7f24aeab379d7ac8a234beccb
Ahnaf Chowdhury <34480805+ahnaf-chowdhury@users.noreply.github.com>
2022-09-05 14:13:39 +0600

    Update wordmaster.py

--- wordmaster.py
+++ wordmaster.py
@@ -2,8 +2,11 @@
 WORDMASTER
 
 A word guessing game similar to Wordle (https://www.nytimes.com/games/wordle/index.html)
-that runs on the command line.
-The user has six tries to guess a five-letter word the program has randomly chosen.
+that runs on the command line, but with a twist.
+
+In the original game, the user has six tries to guess a five-letter word the
+program has randomly chosen. In this version, the user can choose the length of
+words they are playing with, from 3 to 8.
 
 During gameplay, the program highlights letters that are correct or incorrect by
 marking correct letters in the correct position green (and capitalised), correct
@@ -12,15 +15,15 @@ letters in the default font colour of the console.
 
 In addition to displaying the guessed word with letters highlighted appropriately,
 the program also displays a virtual qwerty keyboard that marks used letters with
-a strikethrough, and displays them with the green-yellow-default colout scheme
-that has been described.
+a strikethrough, and displays them with the same green-yellow-default colour
+scheme.
 
 This module contains two classes:
 Keyboard - which helps in displaying the virtual keyboard appropriately, and
 Game - which help initiating and progressing through a game.
 
-The game can be played by running this script directly or by importing the Keyboard
-and Game classes from this module on to another script.
+The game can be played by running this script directly or by importing the module
+(or the Game and Keyboard classes) to another script.
 '''
 
 import enchant
@@ -29,6 +32,27 @@ import random
 import os.path
 from typing import Tuple, List
 import sys
+import json
+
+def get_len_word() -> int:
+    '''
+    Prompts the user for the length of words (3 to 8) they want to play with.
+
+    Parameters: None
+    Returns:
+        int: length of words.
+    '''
+    val = 0
+    while not val:     # exception handling and validation (3<=len<=8)
+        try:
+            val = int(input("Please enter the length of words you would like to play with (3 to 8)"))
+        except ValueError:
+            print("ERROR: The input must be an integer.")
+
+        if val and (val<3 or val>8):
+            print("ERROR: The number must be from 3 to 8.")
+            val = 0
+    return val
 
 class Keyboard:
     '''
@@ -39,8 +63,8 @@ class Keyboard:
 
     The "self.attributes" dictionary contains attribute codes that can be added
     to the keys to visually aid the player.
-
     '''
+
     def __init__(self):
         self.keys = [['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
                     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
@@ -91,6 +115,7 @@ class Game:
     Some class variables:
 
     self.guess: Holds a string (during gameplay) containing the user's guess
+    self.len_word: Length of words in the game
     self.results: Contains results of each of the six guesses the user makes in
         a nested list.
     self.words_list: A list containing a dictionary of five-letter words loaded
@@ -102,12 +127,12 @@ class Game:
     self.dictionary: an object from theh "enchant" library that is used as a
         spellchecker to validate each guess.
     '''
-    def __init__(self):
+    def __init__(self, len_word):
         self.tries = 0
         self.found = False
         self.guess = None
-
-        self.results = [["_" for _ in range(5)] for _ in range(6)]
+        self.len_word = len_word
+        self.results = [["_" for _ in range(len_word)] for _ in range(6)]
         self.kb = Keyboard()
         self.attributes = {'green':'\033[92m',
               'yellow':'\033[93m',
@@ -117,23 +142,20 @@ class Game:
               'strikethrough':'\u0336'}   # use strikethrough after string
 
         self.language = 'en_US'
-        url = "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
-        self.words_list, self.count = self.load_dictionary2list('five_letter_words.txt', url)   # load dictionary of 5-letter words
+        self.words_list, self.count = self.load_dictionary2list('words_dict.txt', self.len_word)   # load dictionary of words of length self.len_word
         self.true_word = self.words_list[random.randint(0, self.count-1)]              # word to guess
         self.true_word_dict = self.word2dict(self.true_word)                       # dictionary containing letter counts of the word to guess
         self.dictionary = enchant.Dict(self.language)                              # spellchecker
 
-    def load_dictionary2list(self, filename:str, download_url:str) -> Tuple[List[str], int]:
+    def load_dictionary2list(self, filename:str, len_word:int) -> Tuple[List[str], int]:
         '''
-        Reads a dictionary of five-letter words from a txt file onto a list.
-        If the file doesn't exist, it tries to download it from a URL.
+        Reads a dictionary of words of desired length from a txt file (saved in
+        json format) onto a list.
 
         Parameters:
             filename (str): The method looks for this filename in the current
-                directory, which should store the words.
-            download_url (str): If the filename mentioned is not found the method
-                tries to download the required data from this URL and saves it
-                under filename.
+                directory, which should store the words in json format.
+            len_word (int): Length of each word that should be in the list.
 
         Returns:
             Tuple(value1, value2):
@@ -141,25 +163,16 @@ class Game:
             value2: the length of this list.
         '''
 
-        dictionary = enchant.Dict(self.language)  # spellchecker
+        try:
+            word_dict = json.load(open(filename))
+        except FileNotFoundError:
+            print("The file \"" + filename + "\" was not found in the current directory.")
+            sys.exit()
+        except:
+            print("Could not load json file from \"" + filename + "\". Please fix this and try again.")
+            sys.exit()
 
-        if not os.path.isfile(filename):      # download file if it's not there
-            res = req.get(download_url)
-
-            if res.status_code == 404:
-                print("ERROR: URL for downloading dictionary \"" + download_url + "\" not found. Please fix this and try again.")    #to check whether the URL is valid
-                sys.exit()
-
-            with open(filename, 'wb') as f:
-                f.write(res.content)
-
-        # opening the file and reading the words onto a list:
-        words_file = open(filename)
-        words_list = []
-        for line in words_file:
-            if dictionary.check(line[:-1]):        # to avoid words that the spellchecker doesnt know of
-                words_list.append(line[:-1])
-        words_file.close()
+        words_list = word_dict[str(len_word)]      # json reads key as string
 
         return words_list, len(words_list)
 
@@ -172,10 +185,10 @@ class Game:
         self.tries = 0
         self.found = False
         self.guess = None
-        self.results = [["_" for _ in range(5)] for _ in range(6)]
+        self.results = [["_" for _ in range(self.len_word)] for _ in range(6)]
         self.kb = Keyboard()
 
-        self.true_word = self.words_list[random.randint(0, self.count)]              # word to guess
+        self.true_word = self.words_list[random.randint(0, self.count-1)]              # word to guess
         self.true_word_dict = self.word2dict(self.true_word)
 
     def play(self) -> None:
@@ -185,8 +198,8 @@ class Game:
         Six tries are allowed.
 
         First, a valid guess is taken in from the user using another method, which
-        ensures that the input is a five-character string which is a dictionary
-        word.
+        ensures that the input is a string of appropriate length which is a
+        dictionary word.
 
         If the guess isn't '0' (exit code), exact matches (correct letters in
         correct positions) are checked for first. Then other matches (correct
@@ -235,14 +248,14 @@ class Game:
 
     def check_exact_matches(self) -> None:
         '''
-        Checks each of the five letters in the guess for an exact match with the
+        Checks each letter in the guess for an exact match with the
         true word. If there is a match, it updates the results and keyboard objects
         to display the appropriate attributes (green colour and strikethrough
         for the keyboard). It also updates the self.true_word_dict dicitonary using
         another method which decrements the count of a certain letter in the
         dicitonary object.
         '''
-        for i in range(5):
+        for i in range(self.len_word):
             if self.guess[i] == self.true_word[i]:
                 self.results[self.tries][i] = self.attributes['green'] + self.attributes['bold'] + self.guess[i].upper() + self.attributes['end']
                 self.kb.mark(self.guess[i], 'green')
@@ -262,7 +275,7 @@ class Game:
         and marked with a strikethrough in the keyboard.
 
         '''
-        for i in range(5):
+        for i in range(self.len_word):
             if self.results[self.tries][i] == "_":                    # an empty position means there is no exact match here
                 if self.guess[i] in self.true_word_dict:              # the letter is available
                     self.results[self.tries][i] = self.attributes['yellow'] + self.attributes['bold'] + self.guess[i].upper() + self.attributes['end']
@@ -275,20 +288,20 @@ class Game:
 
     def get_guess(self) -> str:
         '''
-        Prompts the user to enter a five-letter guess until it gets a five-letter
-        word which is validated by the dictionary.
+        Prompts the user to enter a guess until it gets a word of appropriate
+        length which is validated by the dictionary.
 
         Parameters: None.
         Returns:
-            str: The user's five-letter guess.
+            str: The user's guess.
         '''
 
         guess = None
         while not guess:
-            guess = input('Please enter a guess (5 letters):  (or enter 0 to quit)')
+            guess = input('Please enter a guess (' + str(self.len_word) + ' letters):  (or enter 0 to quit)')
             if guess != '0':
-                if len(guess) != 5:
-                    print('Guess must consist of 5 letters')
+                if len(guess) != self.len_word:
+                    print('Guess must consist of ' + str(self.len_word) + ' letters')
                     guess = None
                 elif not self.dictionary.check(guess):
                     print('Word not recognised')
@@ -297,7 +310,7 @@ class Game:
 
     def word2dict(self, word:str) -> dict:
         '''
-        Converst a string to a dictionary containing the counts of its characters.
+        Converts a string to a dictionary containing the counts of its characters.
 
         Parameters: word (str): the input string.
         Returns: dict.
@@ -325,9 +338,10 @@ class Game:
 
     def print_results(self) -> None:
         '''
-        Prints results in a 5x6 grid
+        Prints results in a grid
         '''
         for line in self.results:
+            # print("     ", end="")
             for ch in line:
                 print(ch, end=" ")
             print("")
@@ -335,8 +349,9 @@ class Game:
 
 if __name__ == "__main__":
 
-    # create game object:
-    this_game = Game()
+    # create game object using length of words.
+    len_word = get_len_word()
+    this_game = Game(len_word)
 
     # play game:
     play = True
